344d4cba58e48a8772f95043fc89c70bc10937a7
Code Craft <codecraft.inc@gmail.com>
2023-01-04 18:45:55 +0000

    first task, contracts by id restricted by profile done

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const {Op} = require('sequelize');
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
+ * Return contracts that belongs to the profile, uses the key depending on the profile type
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+    const {profile} = req
+    const belongToKey = profile.type === 'client' ? 'ClientId' : 'ContractorId'
+
+    const contract = await Contract.findOne({where: { id, [belongToKey]: profile.id}})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
